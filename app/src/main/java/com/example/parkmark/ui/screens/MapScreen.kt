ecb1845a26a123b7124ec416518a547e51afbe3a
Erik Pavelka <pavelka6@stud.uniza.sk>
2026-05-21 15:49:11 +0200

    pridanie tlacidla na mapScreen a ukladanie polohy do databazy

--- app/src/main/java/com/example/parkmark/ui/screens/MapScreen.kt
+++ app/src/main/java/com/example/parkmark/ui/screens/MapScreen.kt
@@ -1,30 +1,180 @@
 package com.example.parkmark.ui.screens
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.NavController
 import com.example.parkmark.viewmodel.ParkingViewModel
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.rememberCameraPositionState
+
 
 @Composable
 fun MapScreen(navController: NavController, viewModel: ParkingViewModel) {
-    val defaultLocation = LatLng(49.2231, 18.7394)
+    val context = LocalContext.current
 
+    val history by viewModel.parkingHistory.collectAsState()
+
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+
+    var hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context, Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            // Ak sa používateľ práve vrátil do aplikácie (napr. z nastavení)
+            if (event == Lifecycle.Event.ON_RESUME) {
+                hasLocationPermission = ContextCompat.checkSelfPermission(
+                    context, Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+
+    val defaultLocation = LatLng(49.2231, 18.7394)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(defaultLocation, 14f)
     }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted -> hasLocationPermission = isGranted}
+    )
+
+    LaunchedEffect(hasLocationPermission) {
+        if (hasLocationPermission) {
+            try {
+                fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                    if (location != null) {
+                        val userLatLng = LatLng(location.latitude, location.longitude)
+                        cameraPositionState.position = CameraPosition.fromLatLngZoom(userLatLng, 16f)
+                    }
+                }
+            } catch (e: SecurityException) {
+
+            }
+        } else {
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+
+    val mapProperties = MapProperties(isMyLocationEnabled = hasLocationPermission)
+    val uiSettings = MapUiSettings(myLocationButtonEnabled = hasLocationPermission)
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
+            cameraPositionState = cameraPositionState,
+            properties = mapProperties,
+            uiSettings = uiSettings
         )
+
+        LargeFloatingActionButton(
+            onClick = {
+                if (hasLocationPermission) {
+                    try {
+                        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                            if (location != null) {
+                                viewModel.saveParking(
+                                    latitude = location.latitude,
+                                    longitude = location.longitude,
+                                    address = "ziadna adresa",
+                                    note = "ziaden note",
+                                    expireTime = null
+                                )
+
+                                Toast.makeText(
+                                    context,
+                                    "Parking location saved",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            } else {
+                                Toast.makeText(
+                                    context,
+                                    "GPS nefunguje. testovaci zaznam zilina",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    } catch (e: SecurityException) {
+                        android.widget.Toast.makeText(context, "Chýba povolenie na polohu", android.widget.Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    // Informujeme používateľa, prečo to nefunguje
+                    android.widget.Toast.makeText(
+                        context,
+                        "Poloha je zakázaná. Povoľte ju v nastaveniach aplikácie.",
+                        android.widget.Toast.LENGTH_LONG
+                    ).show()
+
+                    // Otvoríme systémové nastavenia priamo na detaile našej aplikácie
+                    val intent = android.content.Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                        data = android.net.Uri.fromParts("package", context.packageName, null)
+                    }
+                    context.startActivity(intent)
+                }
+
+
+
+            },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 32.dp),
+                containerColor = MaterialTheme.colorScheme.primary
+
+
+            ) {
+                androidx.compose.material3.Icon(
+                    imageVector = androidx.compose.material.icons.Icons.Default.LocationOn,
+                    contentDescription = "save location",
+                    modifier = Modifier.size(75.dp)
+                )
+
+        }
     }
 }
